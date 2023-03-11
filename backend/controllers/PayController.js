35beac6325638173e35421dd3451c890bc67221c
OrgesMat <orges.69matoshi@gmail.com>
2023-03-11 21:03:41 +0100

    payment for cart

---
+++ backend/controllers/PayController.js
@@ -0,0 +1,49 @@
+
+import PayModel from '../models/Pay';
+import { StatusCodes,  ReasonPhrases  } from 'http-status-codes';
+
+const controller = {
+    list: async(req, res) => {
+        const list = await PayModel.find();
+        return res.json(list);
+    },
+    find: async(req, res) => {
+        try {
+            const category = await PayModel.findOne({ _id: req.params.Id });
+    
+            if (!category) throw Error("Category not found");
+            return res.json(category);
+        } catch (error) {
+            res.status(404).json({ error: error.message })
+        }
+    },
+    create: async(req, res) => {
+        console.log('req.body - ', req.body);
+    
+        if (validationResult.error) {
+            return res
+                .status(StatusCodes.UNAUTHORIZED)
+                .json({
+                    message: ReasonPhrases.UNAUTHORIZED,
+                    error: validationResult.error.message
+                });
+        }
+    
+        const newCategory = new PaytModel(req.body);
+    
+        try {
+            await newCategory.save();
+        
+            return res.json(newCategory);
+        } catch (err) {
+            return res.json(StatusCodes.UNAUTHORIZED)
+                .json({
+                    message: ReasonPhrases.UNAUTHORIZED,
+                    error: err.message
+                });
+        }
+    }
+};
+
+
+export default controller;
